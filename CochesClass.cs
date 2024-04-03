c308b900961c4c4262c091e16079f5bb58b38bf7
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-03 10:25:05 +0200

    Add project files.

---
+++ CochesClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFCLogicaCoches.Models;
+using WFCLogicaCoches.Repositories;
+
+namespace WFCLogicaCoches
+{
+    public class CochesClass : ICochesContract
+    {
+        private RepositoryCoches repo;
+        public CochesClass()
+        {
+            this.repo = new RepositoryCoches();
+        }
+        public Coche FindCoche(int idcoche)
+        {
+            return this.repo.FindCoche(idcoche);
+        }
+
+        public List<Coche> GetCoches()
+        {
+            return this.repo.GetCoches();
+        }
+    }
+}
---
+++ Repositories/RepositoryCoches.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using WFCLogicaCoches.Models;
+
+namespace WFCLogicaCoches.Repositories
+{
+    public class RepositoryCoches
+    {
+        private XDocument document;
+
+        public RepositoryCoches()
+        {
+            //NAMESPACE/CARPETA/DOCUMENTO
+            string resourceName = "WFCLogicaCoches.coches.xml";
+            Stream stream = this.GetType().Assembly
+                .GetManifestResourceStream(resourceName);
+            this.document = XDocument.Load(stream);
+        }
+
+        public List<Coche> GetCoches()
+        {
+            var consulta = from datos in document.Descendants("coche")
+                           select new Coche()
+                           {
+                               IdCoche = int.Parse(datos.Element("idcoche").Value),
+                               Marca = datos.Element("marca").Value,
+                               Modelo = datos.Element("modelo").Value,
+                               Imagen = datos.Element("imagen").Value
+                           };
+            return consulta.ToList();
+        }
+
+        public Coche FindCoche(int idcoche)
+        {
+            return this.GetCoches().FirstOrDefault(x => x.IdCoche == idcoche);
+        }
+
+    }
+}
